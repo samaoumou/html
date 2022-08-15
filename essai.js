af1872c9005b29fb8e4b3f2c78a350498d02f1a0
issa <i.ndiaye@isepdiamniadio.edu.sn>
2022-08-15 13:02:46 +0000

    papa

---
+++ essai.js
@@ -0,0 +1,10 @@
+function change(){
+    let button = document.getElementById("bloc1");
+    button.style.backgroundColor = "yellow";
+    button.style.color = "black";
+}
+function change(){
+    let button = document.getElementById("bloc2");
+    button.style.backgroundColor = "yellow";
+    button.style.color = "black";
+}
--- issa.js
+++ issa.js
@@ -1,7 +1,36 @@
-let a=prompt('entrer une valeur');
-if (a<3){
-    alert("positif");
-}
-else
-    alert(negatif);
-    
+let color=['white','brown','yellow','red','green'];
+let button=document.getElementById('button');
+button.addEventListener('click',function couleur(){
+    var randomColor=color[Math.floor(Math.random()*color.length)]
+    let bloc1=document.getElementById('bloc1');
+    if(randomColor=='white'){
+        bloc1.style.color='black';
+    }
+    else{
+        bloc1.style.color='black';
+    }
+    let bloc2=document.getElementById('bloc2');
+    if(randomColor=='white'){
+        bloc2.style.color='black'; 
+    }
+    else{
+        bloc3.style.color='black';
+    }
+    let bloc3=document.getElementById('bloc3');
+    if(randomColor=='white'){
+        bloc3.style.color='black';
+    }
+    else{
+        bloc3.style.color='black';
+    }
+    let bloc4=document.getElementById('bloc4');
+    if(randomColor=='white'){
+        bloc4.style.color='black';
+    }
+    else{
+        bloc4.style.color='black';
+    }
+})     
+
+
+   
